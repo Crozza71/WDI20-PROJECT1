8ed0364e01405c5a763e775ff49453d340efd430
Paul Crosby <paul71crosby@gmail.com>
2016-05-08 14:02:44 +0100

    updates scores working. working on newbowl, not yet working

--- project.js
+++ project.js
@@ -4,129 +4,146 @@
 // else
 // youve missed the ballx
 
+// first let bowler choose what speed to bowl. Listen for keypress on 3 keys
+
 $(init);
 
 function init() {
 
-  document.getElementById("batsman").addEventListener("click", function() {
-    alert("I've been clicked too!");
-          })
-    
-        }
+  
+  var isPlayerOnesTurnAtBat = true;
+  var player1Score = 0;
+  var player2Score = 0;
+      $("#reset").click(function(e) {
+        console.log("ive been clicked");
+      })
+      $(document).on('keydown', function(e){
+          // console.log(e);
 
-        function test(thingtoprint){
-          console.log("this speed should be" + thingtoprint)
-        }
+          switch(e.which){
+            
+            case 81: 
+                      $('#ball').animate({
+                        left: "570px"
+                      }, 500 , handleMiss) // handleMiss gets run if the animation completes.  
 
+            break;
+            
+            case 87:
+                       $('#ball').animate({
+                        left:"570px"
+                       }, 1200, handleMiss)
+            break;
+            
+            case 69:
+                      $('#ball') .animate({
+                        left:"570px"
+                      }, 1800 , handleMiss)         
+            break;  
+
+            case 32:
+                    // $('#ball').animate({
+
+                    // })  // the batsman has taken a swing. find out what his score was
+                      // keep the game logic here
+                      // probably want to stop the current animation too for the moment
+                      // https://api.jquery.com/stop/
+
+                      // get the batsman's score
+                      var score = getScore();
+
+                      // player got some runs
+                      updateScores(score);
+
+                      // next turn
+                      nextBowl();
+
+            break;      
+          }
+      });
+
+
+     function nextBowl(){
+        $('#reset').click(function(){
+          $('#ball').animate({
+            left: "570px"
+          }, 0 )
+          // alert("clicked again");
+        // $('#ball').animate({
+        //                 left:"570px"
+
+      });
+      //  if (ballPosition >= 560 ){
+
+      //   alert (" You're OUT!!!");
+
+      //   else  {
+      //     $('#ball') .animate({
+      //                   left:"570px"
+      //   };
+      // }
+
+
+
+        // reset the ball to the start ( change the left position on the ball back to 0)
+
+
+     }
+
+     function updateScores(score) {$(score).player1Score++;
+
+        alert("You got " + score + "run(s)!" + "finally you knobhead");
+
+     }
+     // player got some runs
+             // update the current players score
+             // tell the user how many runs they got
+             // update the scoreboard on the screen
+
+     function handleMiss() {
+
+        // player missed the ball
+        // tell them they missed and laugh at them
+        // time to swap players
+        // reset the ball again
+        alert('you missed dumbass');
+        nextBowl();
+
+     }
+ 
+
+    function getScore () {
         
+        var ballPosition = parseInt($("#ball").css("left"));  
 
-  $(document).on('keydown', function(e){
-      // console.log(e);
+        if (ballPosition <= 384) { 
+          alert ("That was rubbish!")
+           return 0;
+        } else if (ballPosition <= 449) { 
+           alert ("Tricky , 2 runs!");
+           return 2;
+        } else if (ballPosition <= 469) { 
+           alert ("Great shot, 6 runs!!");
+           return 6;
+        } else if (ballPosition <= 509) { 
+           alert ("Well played , 4 runs!!");
+           return 4;
+        } else if (ballPosition <= 559 ) { 
+           alert (" Run, Run , Run...you got a single, that was close");
+           return 1;
+         } 
 
-      switch(e.which){
-        case 81: 
-                  $('#ball').animate({
-                    left: "570px"
-                  }, 500)
-
-        break;
-        case 87:
-                   $('#ball').animate({
-                    left:"570px"
-                   }, 1200)
-        break;
-        case 69:
-                  $('#ball') .animate({
-                    left:"570px"
-                  }, 1800)         
-         break;  
-         case 32:
-                  checkScore();
-         break;      
-      }
-  });
-
- function checkScore () {
-        
-        var score = parseInt($("#ball").css("left"));  
-        console.log(score);
-
-
-        getScore(score);
+    }
 
  }
-
- function getScore(score) {
-      if (score <= 384){
-      alert ("no run");
-      return(0);
-        } 
-      else if (score <= 449) {
-      alert ("Tricky , 2 runs!");
-      return(2);
-        }
-      else if (score <= 469){
-      alert ("Great shot, 6 runs!!");
-      return(6);
-        }
-      else if (score <= 509) {
-      alert ("Well played , 4 runs!!");
-      return(4);
-        }
-      else if (score <= 559 ) {
-      alert (" Run, Run , Run...you got a single, that was close");
-      return(1)
-        }
-      else 
-      { alert (" That was tough.....you got bowled, you are OUT!!!")
-        return("OUT!! Next batsman please")
-      }
-        
-
- }
-
-  // function getScore () {
-        
-  //       var score = parseInt($("#ball").css("left"));  
-  //       console.log(score);
-
-
-  //       getScore(score);
-    
-// function scoreTotal(score)  {
-//   if (score <= 384){
-//   console.log(0) ;
-//     } 
-//   else if (score <= 449) {
-//   console.log (2);
-//     }
-//   else if (score <= 469){
-//  console.log (6);
-//     }
-//   else if (score <= 509) {
-//   console.log (4);
-//     }
-//   else if (score <= 559 ) {
-//   console.log (1);
-//     }
-//   else 
-//   {console.log(7)}
-    
-// }
-
-// scoreTotal()
-
-// function reset(computer,player1) {
-
-
-// }
-
-// var umpire = {
-//   player1 = 0,
-//   computer = 0
-// }
-
-
+ // get the current location of the ball ( css left position ) and decide what score the batsman gets
+         // scoring - need to build these with an if statement and some comparisons
+         // 0 - 384 - no runs for you
+         // 385 - 449 - 2 runs
+         // 450 - 469 - 6 runs
+         // 470 - 509 - 4 runs
+         // 510 - 559 - 1 run
+         // 559 and over - bowled out
 
 // var batsman=
 // function bowler () {
