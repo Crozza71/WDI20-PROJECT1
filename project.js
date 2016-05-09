3e9896d56faafa9cb813f1b5bd5097b8ffc37f79
Paul Crosby <paul71crosby@gmail.com>
2016-05-09 09:35:39 +0100

    adding new work to index.js

--- project.js
+++ project.js
@@ -4,19 +4,18 @@
 // else
 // youve missed the ballx
 
-// first let bowler choose what speed to bowl. Listen for keypress on 3 keys
-
 $(init);
 
 function init() {
 
-  
+  // first let bowler choose what speed to bowl. Listen for keypress on 3 keys
   var isPlayerOnesTurnAtBat = true;
   var player1Score = 0;
   var player2Score = 0;
-      $("#reset").click(function(e) {
-        console.log("come on , reset");
-      })
+        $("#reset").click(function(e) {
+                console.log("come on , reset");
+              })
+
       $(document).on('keydown', function(e){
           // console.log(e);
 
@@ -25,7 +24,7 @@ function init() {
             case 81: 
                       $('#ball').animate({
                         left: "570px"
-                      }, 350 , handleMiss) // key "q" .handleMiss gets run if the animation completes.  
+                      }, 350 , handleMiss) // handleMiss gets run if the animation completes. 
 
             break;
             
@@ -33,20 +32,20 @@ function init() {
                        $('#ball').animate({
                         left:"570px"
                        }, 800, handleMiss)
-                       // key "w"
             break;
             
             case 69:
                       $('#ball') .animate({
                         left:"570px"
-                      }, 1000 , handleMiss)
-                      // key "e"         
+                      }, 1000 , handleMiss)         
             break;  
 
             case 32:
-                    // $('#ball').animate({
+                      // $('#ball').animate({
 
-                    // })  // the batsman has taken a swing. find out what his score was
+                      // })                      
+
+                      // the batsman has taken a swing. find out what his score was
                       // keep the game logic here
                       // probably want to stop the current animation too for the moment
                       // https://api.jquery.com/stop/
@@ -66,39 +65,24 @@ function init() {
 
 
      function nextBowl() {
-        $('#ball') .animate({
-        right:"570px"}, 1000 , handleMiss)
-          // alert("clicked again");
-     } 
-      
-
-      };
-      //  if (ballPosition >= 560 ){
-
-      //   alert (}" You're OUT!!!");
-
-      //   else  {
-      //     $('#ball') .animate({
-      //                   left:"570px"
-      //   };
-      // }
-
-
+      $('#ball') .animate({
+      left:"0px"}
 
         // reset the ball to the start ( change the left position on the ball back to 0)
 
 
-     // 
+     )}
 
-     function updateScores(score) {$(score).player1Score++;
+     function updateScores(score) {
+      $(score).player1Score++;
 
+        // player got some runs
+        // update the current players score
+        // tell the user how many runs they got
+        // update the scoreboard on the screen
         alert("You got " + score + "run(s)!" + "finally you knobhead");
 
      }
-     // player got some runs
-             // update the current players score
-             // tell the user how many runs they got
-             // update the scoreboard on the screen
 
      function handleMiss() {
 
@@ -114,36 +98,37 @@ function init() {
 
     function getScore () {
         
+        // get the current location of the ball ( css left position ) and decide what score the batsman gets
         var ballPosition = parseInt($("#ball").css("left"));  
 
-        if (ballPosition <= 384) { 
-          alert ("That was rubbish!")
+        // scoring - need to build these with an if statement and some comparisons
+        // 0 - 384 - no runs for you
+        // 385 - 449 - 2 runs
+        // 450 - 469 - 6 runs
+        // 470 - 509 - 4 runs
+        // 510 - 559 - 1 run
+        // 559 and over - bowled out
+
+        if (ballPosition <= 384) { // this is good
            return 0;
-        } else if (ballPosition <= 449) { 
+        } else if (ballPosition <= 449) { // this is good
            alert ("Tricky , 2 runs!");
            return 2;
-        } else if (ballPosition <= 469) { 
+        } else if (ballPosition <= 469) { // this is good
            alert ("Great shot, 6 runs!!");
            return 6;
-        } else if (ballPosition <= 509) { 
+        } else if (ballPosition <= 509) { // this is good
            alert ("Well played , 4 runs!!");
            return 4;
-        } else if (ballPosition <= 559 ) { 
+        } else if (ballPosition <= 559 ) { // this is good
            alert (" Run, Run , Run...you got a single, that was close");
            return 1;
          } 
 
     }
 
- 
- // get the current location of the ball ( css left position ) and decide what score the batsman gets
-         // scoring - need to build these with an if statement and some comparisons
-         // 0 - 384 - no runs for you
-         // 385 - 449 - 2 runs
-         // 450 - 469 - 6 runs
-         // 470 - 509 - 4 runs
-         // 510 - 559 - 1 run
-         // 559 and over - bowled out
+ }
+
 
 // var batsman=
 // function bowler () {
