623016b36f20a899ba0fa10dca01d20f25af0632
Paul Crosby <paul71crosby@gmail.com>
2016-05-09 10:31:05 +0100

    player 1 and player 2 game works

--- project.js
+++ project.js
@@ -12,38 +12,40 @@ function init() {
   var isPlayerOnesTurnAtBat = true;
   var player1Score = 0;
   var player2Score = 0;
-        $("#reset").click(function(e) {
-                console.log("come on , reset");
-              })
+  $("#reset").click(function(e) {})
 
-      $(document).on('keydown', function(e){
+
+  // Getting my elements to update the score
+  var $display = $('.display');
+  var $playerOneScore = $('.playerOneScore');
+  var $playerTwoScore = $('.playerTwoScore'); 
+
+  $(document).on('keydown', function(e){
           // console.log(e);
 
           switch(e.which){
-            
+
             case 81: 
-                      $('#ball').animate({
-                        left: "570px"
+            $('#ball').animate({
+              left: "570px"
                       }, 350 , handleMiss) // handleMiss gets run if the animation completes. 
 
             break;
             
             case 87:
-                       $('#ball').animate({
-                        left:"570px"
-                       }, 800, handleMiss)
+            $('#ball').animate({
+              left:"570px"
+            }, 800, handleMiss)
             break;
             
             case 69:
-                      $('#ball') .animate({
-                        left:"570px"
-                      }, 1000 , handleMiss)         
+            $('#ball') .animate({
+              left:"570px"
+            }, 1000 , handleMiss)         
             break;  
 
             case 32:
-                      // $('#ball').animate({
-
-                      // })                      
+                      $('#ball').stop(true);                   
 
                       // the batsman has taken a swing. find out what his score was
                       // keep the game logic here
@@ -59,33 +61,43 @@ function init() {
                       // next turn
                       nextBowl();
 
-            break;      
-          }
-      });
+                      break;      
+                    }
+                  });
 
 
-     function nextBowl() {
-      $('#ball') .animate({
+  function nextBowl() {
+    $('#ball') .animate({
       left:"0px"}
 
         // reset the ball to the start ( change the left position on the ball back to 0)
 
 
-     )}
+        )}
 
-     function updateScores(score) {
-      $(score).player1Score++;
+    function updateScores(score) {
 
+      if (isPlayerOnesTurnAtBat) {
+          player1Score += score;
+          $playerOneScore.html(player1Score);
+      } else {
+        player2Score += score;
+        $playerTwoScore.html(player2Score);
+      }
         // player got some runs
         // update the current players score
         // tell the user how many runs they got
         // update the scoreboard on the screen
         alert("You got " + score + "run(s)!" + "finally you knobhead");
 
-     }
+        $display.html("Batsman hit " + score)
 
-     function handleMiss() {
+      }
 
+      function handleMiss() {
+
+
+        isPlayerOnesTurnAtBat = !isPlayerOnesTurnAtBat;
         // player missed the ball
         // tell them they missed and laugh at them
         // time to swap players
@@ -93,11 +105,11 @@ function init() {
         alert('you missed dumbass');
         nextBowl();
 
-     }
- 
+      }
 
-    function getScore () {
-        
+
+      function getScore () {
+
         // get the current location of the ball ( css left position ) and decide what score the batsman gets
         var ballPosition = parseInt($("#ball").css("left"));  
 
@@ -110,24 +122,24 @@ function init() {
         // 559 and over - bowled out
 
         if (ballPosition <= 384) { // this is good
-           return 0;
+         return 0;
         } else if (ballPosition <= 449) { // this is good
-           alert ("Tricky , 2 runs!");
-           return 2;
+         alert ("Tricky , 2 runs!");
+         return 2;
         } else if (ballPosition <= 469) { // this is good
-           alert ("Great shot, 6 runs!!");
-           return 6;
+         alert ("Great shot, 6 runs!!");
+         return 6;
         } else if (ballPosition <= 509) { // this is good
-           alert ("Well played , 4 runs!!");
-           return 4;
+         alert ("Well played , 4 runs!!");
+         return 4;
         } else if (ballPosition <= 559 ) { // this is good
-           alert (" Run, Run , Run...you got a single, that was close");
-           return 1;
-         } 
+         alert (" Run, Run , Run...you got a single, that was close");
+         return 1;
+       } 
 
-    }
+     }
 
- }
+   }
 
 
 // var batsman=
